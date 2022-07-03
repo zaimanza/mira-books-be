6c80b4a940d38c59c12b3e59a46aa110dbaf8ccf
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-07-03 20:09:54 +0800

    encoded true

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { connectDB } = require('./database/mongodb.database');
 
 connectDB()
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors())
 
