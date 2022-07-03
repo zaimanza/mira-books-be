96c3a4d9a30cad4c66167daec7c6acbf9bd70814
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-07-03 18:42:48 +0800

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http')
 
 const express = require("express");
+var cors = require('cors')
 const app = express()
 const bodyParser = require("body-parser");
 const { connectDB } = require('./database/mongodb.database');
@@ -9,6 +10,7 @@ connectDB()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
